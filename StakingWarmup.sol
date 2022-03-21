a898971e1bcf3b8a62d680239ef54714d50e78b2
BROWSER COMPANY.COM <61902110+P7-33@users.noreply.github.com>
2022-03-21 09:26:46 -0500

    Create StakingWarmup.sol

---
+++ StakingWarmup.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.7.5;
+
+import "./libs/IERC20.sol";
+
+//modified from OlympusDao
+//author : _bing @ MetaversePRO
+
+contract StakingWarmup {
+
+    address public immutable staking;
+    address public immutable sMETA;
+
+    constructor ( address _staking, address _sMETA ) {
+        require( _staking != address(0) );
+        staking = _staking;
+        require( _sMETA != address(0) );
+        sMETA = _sMETA;
+    }
+
+    function retrieve( address _staker, uint _amount ) external {
+        require( msg.sender == staking );
+        IERC20( sMETA ).transfer( _staker, _amount );
+    }
+}
