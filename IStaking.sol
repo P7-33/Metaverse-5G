c1318ef2a84651b52835d8687123bfc6189a9cca
P7-33 <61902110+P7-33@users.noreply.github.com>
2022-04-10 17:11:44 -0500

    Update IStaking.sol

--- IStaking.sol
+++ IStaking.sol
@@ -1,14 +1,3 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.7.5;
-
-interface IStaking {
-    function stake( uint _amount, address _recipient ) external returns ( bool );
-    function claim( address _recipient ) external;
-	function unstake( uint _amount, bool _trigger ) external;
+// jhh
 }
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
+
