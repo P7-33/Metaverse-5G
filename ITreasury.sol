c97d161aa7acc99e52e55f85dcbe9a8d2a03afca
P7-33 <61902110+P7-33@users.noreply.github.com>
2022-04-10 16:57:24 -0500

    Delete ITreasury.sol

--- ITreasury.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.7.5;
-
-interface ITreasury {
-    function mintRewards( address _recipient, uint _amount ) external;
-	function deposit( uint _amount, address _token, uint _profit ) external returns ( bool );
-    function valueOf( address _token, uint _amount ) external view returns ( uint value_ );
-}
